93787c6cb1f01670114ae71d40e4de2a59ecb3c8
bhaskarroberts <bhaskarroberts@gmail.com>
2018-04-27 11:51:57 -0400

    Add files via upload

--- Applications/app_1.py
+++ Applications/app_1.py
@@ -8,9 +8,10 @@ while True:
     # Step 2: Calculate Packet Fields
     velocities = (1, 1, 1) # Desired speed of each car
     colors = ((255,0,0),(0,255,0),(0,0,255)) # RGB colors of each car's LED
+    error_vectors = car_vectors # Format: ([tx1,ty1,r1], [tx2,ty2,r2], [tx3,ty3,r3])
 
     # Step 3: Transmit Packet
-    cam.tx_pkt(car_vectors, velocities, colors)
+    cam.tx_pkt(error_vectors, velocities, colors)
 
     
     if cam.cv2.waitKey(1000/framerate) & 0xFF == ord('q'):
